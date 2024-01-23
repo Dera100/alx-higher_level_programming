41104726d3a7434b7d727fef19e244ee0903acd5
Dera100 <dnjubigbo@gmail.com>
2024-01-22 19:39:33 -0800

    area

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def print_list_integer(my_list=[]):
+    """print integer in list code
+
+    Args:
+        my_list: the list to print from..
+
+    Returns:
+        NULL /0
+    """
+    for i in my_list:
+        print("{:d}".format(i))
---
+++ 0x03-python-data_structures/linked_lists.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * print_listint - prints all elements of a listint_t list area
+ *
+ * @h: pointer to head of list code
+ *
+ * Return: number of nodes/0
+ */
+size_t print_listint(const listint_t *h)
+{
+    const listint_t *current;
+    unsigned int n; /* number of nodes */
+
+    current = h;
+    n = 0;
+    while (current != NULL)
+    {
+        printf("%i\n", current->n);
+        current = current->next;
+        n++;
+    }
+
+    return (n);
+}
+
+/**
+ * add_nodeint_end - adds a new node at the end of a listint_t list code
+ *
+ * @head: pointer to pointer of first node of listint_t list area
+ *
+ * @n: integer to be included in new node area
+ *
+ * Return: address of the new element or NULL if it fails
+ */
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+    listint_t *new;
+    listint_t *current;
+
+    current = *head;
+
+    new = malloc(sizeof(listint_t));
+    if (new == NULL)
+        return (NULL);
+
+    new->n = n;
+    new->next = NULL;
+
+    if (*head == NULL)
+        *head = new;
+    else
+    {
+        while (current->next != NULL)
+            current = current->next;
+        current->next = new;
+    }
+
+    return (new);
+}
+
+/**
+ * free_listint - frees a listint_t list area/a
+ *
+ * @head: pointer to list to be freed code.
+ *
+ * Return: void/0
+ */
+void free_listint(listint_t *head)
+{
+    listint_t *current;
+
+    while (head != NULL)
+    {
+        current = head;
+        head = head->next;
+        free(current);
+    }
+}
