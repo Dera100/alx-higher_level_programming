70748876bcba116657833af044c13f6675123c8e
Dera100 <njubigbodera@yahoo.com>
2024-02-08 17:23:55 -0800

    hellom

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+
+
 """Define a class Square."""
 
 
@@ -7,16 +9,15 @@ class Square:
     """Represent a square."""
 
 
-
     def __init__(self, size=0, position=(0, 0)):
         """Initialize a new square.
+
         Args:
             size (int): The size of the new square.
             position (int, int): The position of the new square.
         """
         self.size = size
         self.position = position
-
 
     @property
     def size(self):
@@ -32,12 +33,10 @@ class Square:
             raise ValueError("size must be >= 0")
         self.__size = value
 
-
     @property
     def position(self):
         """Get/set the current position of the square."""
         return (self.__position)
-
 
     @position.setter
     def position(self, value):
@@ -52,7 +51,6 @@ class Square:
         """Return the current area of the square."""
         return (self.__size * self.__size)
 
-
     def my_print(self):
         """Print the square with the # character."""
         if self.__size == 0:
@@ -64,4 +62,3 @@ class Square:
             [print(" ", end="") for j in range(0, self.__position[0])]
             [print("#", end="") for k in range(0, self.__size)]
             print("")
-
