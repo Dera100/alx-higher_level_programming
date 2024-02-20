b48c9f108518fb20ea76d717ea96c86b94b64123
Dera100 <njubigbodera@yahoo.com>
2024-02-19 19:51:56 -0800

    jjj

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Defines a base geometry class BaseGeometry."""
+
+
+class BaseGeometry:
+    """Represent base geometry code."""
+
+    def area(self):
+        """Not implemented code."""
+        raise Exception("area() is not implemented")
