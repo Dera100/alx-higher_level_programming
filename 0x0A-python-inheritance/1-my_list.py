67321c06be83ff23d57e1e3d1b2fc354a69938de
Dera100 <njubigbodera@yahoo.com>
2024-02-19 19:37:59 -0800

    kkik

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+3
+"""
+contains the MyList class list
+"""
+
+
+
+class MyList(list):
+    """a subclass of list"""
+    def __init__(self):
+        """initializes the object"""
+        super().__init__()
+
+
+    def print_sorted(self):
+        """prints the sorted list"""
+        print(sorted(self))
